71f3db87fa8ba2826f8eed83c0def499d1289b98
Glutencio <barriosurielivan@gmail.com>
2024-11-24 00:25:36 -0300

    Prueba de un commit

--- MyFirstProyectCSharp/Program.cs
+++ MyFirstProyectCSharp/Program.cs
@@ -11,7 +11,7 @@ namespace MyFirstProyectCSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello Word");
-            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
